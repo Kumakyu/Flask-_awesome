ba39b2fdb9af3d4410ff2eb43e4a1407925cab9d
NaoFujimoto <fujimoto.nao.wq@mynavi.jp>
2024-05-28 08:48:39 +0000

    Flask２日目終わり

--- fujimoto/application/flask_blog/views.py
+++ fujimoto/application/flask_blog/views.py
@@ -1,21 +1,30 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
+import sys
+args = sys.argv
+
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')
     return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method=='POST':
         if request.form['username']!=app.config['USERNAME']:
-            print('ユーザ名が異なります')
+            flash('ユーザ名が異なります')
         elif request.form['password']!=app.config['PASSWORD']:
-            print('パスワードが異なります')
-    else:
-        return redirect('/')
+            flash('パスワードが異なります')
+        else:
+            session['logged_in']=True
+            flash('ログインしました')
+            return redirect('/')
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+    session.pop('logged_in', None)
+    flash('ログアウトしました')
     return redirect('/')
