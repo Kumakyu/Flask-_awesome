1b7510ad16ac4e2048d46de5d815d4882dc23f50
Kumakyu <takahashi.yusuke.ma@mynavi.jp>
2024-05-29 02:55:20 +0000

    給料計算アプリ計算

--- takahashi/calcsalary_app/salary/__init__.py
+++ takahashi/calcsalary_app/salary/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+# from flask_cors import CORS
 
-app=Flask(__name__)
-app.config.from_object('flask_blog.config')
+app = Flask(__name__)
+"""
+app = Flask(__name__, static_folder="./build/static", template_folder="./build")
+CORS(app) #Cross Origin Resource Sharing
+"""
 
+app.config.from_object("salary.config")
 import salary.views
